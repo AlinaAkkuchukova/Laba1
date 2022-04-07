ef40b573ac8088a243ddd0c1c93b093f0c6638bb
Алина Аккучукова <lina.ackuchukova@yandex.ru>
2022-04-07 17:16:57 +0500

    Point

--- Laba1.cpp
+++ Laba1.cpp
@@ -1,11 +1,30 @@
 ﻿#include<stdio.h>
 #include<iostream>
+#include<GL/glew.h>
 #include<GL/freeglut.h>
+#include <glm/vec3.hpp>
+
+GLuint VBO;
 
 static void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_POINTS, 0, 1);
+	glDisableVertexAttribArray(0);
+
 	glutSwapBuffers();
+}
+
+static void CreateVertexBuffer() {
+	glm::vec3 Vertices[1];
+	Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 
 int main(int argc, char** argv) {
@@ -20,11 +39,15 @@ int main(int argc, char** argv) {
 
 	glutDisplayFunc(RenderSceneCB);
 
-	glClearColor(1.0f, 1.0f, 0.0f, 0.0f);
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
 
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-	glutSwapBuffers();
+	CreateVertexBuffer();
 	glutMainLoop();
 
 	return 0;
